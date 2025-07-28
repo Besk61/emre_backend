b45688eec8564d2be3c10ec3cc581b31a1c7135a
Bora Eskicioğlu <76914128+Besk61@users.noreply.github.com>
2025-07-28 10:37:02 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 # 🔴 YENİ: strict_slashes=False tüm uygulama için ayarlanabilir.
 # Bu, /rota ve /rota/ adreslerinin aynı şekilde davranmasını sağlar.
 app.url_map.strict_slashes = False
-port = 5000
+port = 80
 
 # Orijin tanımı (CORS)
 # CORS(app, 
